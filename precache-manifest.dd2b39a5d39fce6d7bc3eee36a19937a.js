2b12531fd469e9cf0c2c243bbb1b4422fb45f790
Felipe Cipolato <felipe.cipolato@accurate.com.br>
2020-09-18 11:03:12 -0300

    Updates

---
+++ precache-manifest.dd2b39a5d39fce6d7bc3eee36a19937a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eb67be5231e00047488d743e77b89c",
+    "url": "/simple-store/index.html"
+  },
+  {
+    "revision": "cd581495230a080e874a",
+    "url": "/simple-store/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "cd581495230a080e874a",
+    "url": "/simple-store/static/js/2.b6ebc2f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/simple-store/static/js/2.b6ebc2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a93d20a351979e295e7",
+    "url": "/simple-store/static/js/main.29cf5a1a.chunk.js"
+  },
+  {
+    "revision": "0aeb83081a15d70ca52f",
+    "url": "/simple-store/static/js/runtime-main.3950ab9d.js"
+  },
+  {
+    "revision": "757b71f9d46f12bccb4ce579e182354f",
+    "url": "/simple-store/static/media/cart.757b71f9.svg"
+  },
+  {
+    "revision": "d1abacc54368a36770be4178aab9f6bd",
+    "url": "/simple-store/static/media/home.d1abacc5.svg"
+  },
+  {
+    "revision": "ef557ebffaaaa2f811d67ed07e498fc6",
+    "url": "/simple-store/static/media/not-found.ef557ebf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35eac668d472a56d646b1cf2c57645eb.js"
+  "/simple-store/precache-manifest.dd2b39a5d39fce6d7bc3eee36a19937a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
